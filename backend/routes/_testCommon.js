2b0fceb9d45dd47a6274238c4aba15ecf9eeef73
Meryl <meryldominguez@gmail.com>
2021-07-27 11:47:18 -0500

    reorganized db calls in routes testcommon

--- backend/routes/_testCommon.js
+++ backend/routes/_testCommon.js
@@ -2,228 +2,40 @@
 
 
 const db = require('../db.js')
-const User = require('../models/user')
-const Lot = require('../models/lot')
+
 const Location = require('../models/Location')
-const { createToken } = require('../helpers/tokens')
+const Lot = require('../models/lot')
 const Production = require('../models/production.js')
 const Prop = require('../models/prop.js')
+const Tag = require("../models/tag")
+const User = require('../models/user')
+
+const { createToken } = require('../helpers/tokens')
+
 
 async function commonBeforeAll () {
   // noinspection SqlWithoutWhere
-  await db.query('DELETE FROM lot')
-  await db.query('DELETE FROM users')
   await db.query('DELETE FROM location')
-  await db.query('DELETE FROM tag')
+  await db.query('DELETE FROM lot')
   await db.query('DELETE FROM production')
   await db.query('DELETE FROM prop')
-
-  const warehouse = await Location.create(
-    {
-      name: 'Warehouse',
-      notes: 'warehouse address, number of coordinator',
-      parentId: null
-    }
-  )
-  const studio = await Location.create(
-    {
-      name: "Rehearsal studio",
-      notes: "studio address, number of coordinator",
-      parentId: null
-    }
-  )
-
-  const bay1 = await Location.create(
-    {
-      name: "Bay 1",
-      notes: null,
-      parentId: warehouse.id
-    }
-  )
-  const bay2 =await Location.create(
-    {
-      name: "Bay 2",
-      notes: null,
-      parentId: warehouse.id
-    }
-  )
+  await db.query('DELETE FROM tag')
+  await db.query('DELETE FROM users')
   
-  const lot1 = await Lot.create(
-      {
-        name: "Lot1",
-        description: "New Lot1",
-        quantity: 3,
-        locId: bay1.id,
-        price : "$20.99"
-      });
+  await addLocations();
+  console.log("added Locations")
+  await addLots();
+  console.log("added Lots")
+  await addProductions();
+  console.log("added Productions")
+  await addProps();
+  console.log("added Props")
+  await addTags();
+  console.log("No tags yet")
 
-  const lot2 = await Lot.create(
-      {
-        name: "Lot2",
-        description: "New Lot2",
-        quantity: 50,
-        locId: bay1.id,
-        price : null
-      });
-  const lot3 = await Lot.create(
-      {
-        name: "Lot3",
-        description: "New Lot3",
-        quantity: 1,
-        locId: bay1.id,
-        price : "$50"
-      });
-  const lot4 = await Lot.create(
-      {
-        name: "Lot4",
-        description: "New Lot4",
-        quantity: 10,
-        locId: bay2.id,
-        price : "$10.99"
-      });
-  const lot5 = await Lot.create(
-      {
-        name: "Lot5",
-        description: "New Lot5",
-        quantity: 50,
-        locId: bay2.id,
-        price : null
-      });
-  const lot6 = await Lot.create(
-      {
-        name: "Lot6",
-        description: "New Lot6",
-        quantity: 300,
-        locId: bay2.id,
-        price : "$2"
-      });
-  const lot7 = await Lot.create(
-      {
-        name: "Lot7",
-        description: "New Lot7",
-        quantity: null,
-        locId: bay2.id,
-        price : "$30"
-      });
-  const lot8 = await Lot.create(
-      {
-        name: "Lot8",
-        description: "New Lot8",
-        quantity: null,
-        locId: studio.id,
-        price : null
-      });
+  await addUsers();
+  console.log("added Users")
 
-  const prod1 = await Production.create({
-    title:"Carmen",
-    dateStart: new Date("May, 2020"),
-    dateEnd: new Date("July, 2020"),
-    active:true,
-    notes:"a test production"
-  });
-  const prod2 = await Production.create({
-    title:"La traviata",
-    dateStart: new Date("September, 2006"),
-    dateEnd: new Date("November, 2006"),
-    active:false,
-    notes:"a second test production"
-  });
-  const prod3 = await Production.create({
-    title:"The magic flute",
-    dateStart: new Date("January, 2001"),
-    dateEnd: new Date("December, 2003"),
-    active:true,
-    notes:"a third test production"
-  });
-
-  await Prop.create({
-    prodId:prod1.id,
-    lotId:lot1.id,
-    quantity:2,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod1.id,
-    lotId:lot5.id,
-    quantity:20,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod1.id,
-    lotId:lot3.id,
-    quantity:2,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod2.id,
-    lotId:lot8.id,
-    quantity:null,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod2.id,
-    lotId:lot6.id,
-    quantity:100,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod2.id,
-    lotId:lot1.id,
-    quantity:2,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod3.id,
-    lotId:lot3.id,
-    quantity:2,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod3.id,
-    lotId:lot7.id,
-    quantity:300,
-    notes:"prop notes"
-  })
-  await Prop.create({
-    prodId:prod3.id,
-    lotId:lot5.id,
-    quantity:10,
-    notes:"prop notes"
-  })
-
-
-  await User.register({
-    username: 'u1',
-    firstName: 'U1F',
-    lastName: 'U1L',
-    email: 'user1@user.com',
-    password: 'password1',
-    isAdmin: false
-  })
-  await User.register({
-    username: 'u2',
-    firstName: 'U2F',
-    lastName: 'U2L',
-    email: 'user2@user.com',
-    password: 'password2',
-    isAdmin: false
-  })
-  await User.register({
-    username: "u3",
-    firstName: "U3F",
-    lastName: "U3L",
-    email: "user3@user.com",
-    password: "password3",
-    isAdmin: false,
-  });
-  await User.register({
-    username: "a1",
-    firstName: "A1F",
-    lastName: "A1L",
-    email: "admin@user.com",
-    password: "adminpassword1",
-    isAdmin: true
-  });
 };
 
 async function commonBeforeEach() {
@@ -236,6 +48,262 @@ async function commonAfterEach() {
 
 async function commonAfterAll() {
   await db.end();
+};
+
+async function addLocations(){
+  try {
+    const warehouse = await Location.create(
+      {
+        name: 'Warehouse',
+        notes: 'warehouse address, number of coordinator',
+        parentId: null
+      }
+    )
+    await Location.create(
+      {
+        name: "Rehearsal studio",
+        notes: "studio address, number of coordinator",
+        parentId: null
+      }
+    )
+  
+    await Location.create(
+      {
+        name: "Bay 1",
+        notes: null,
+        parentId: warehouse.id
+      }
+    )
+    await Location.create(
+      {
+        name: "Bay 2",
+        notes: null,
+        parentId: warehouse.id
+      }
+    )
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+async function addLots(){
+  try {
+    const [warehouse,studio,bay1,bay2] = await Location.getChildren()
+
+    await Lot.create(
+      {
+        name: "Lot1",
+        description: "New Lot1",
+        quantity: 3,
+        locId: bay1.locationId,
+        price : "$20.99"
+      });
+    await Lot.create(
+      {
+        name: "Lot2",
+        description: "New Lot2",
+        quantity: 50,
+        locId: bay1.locationId,
+        price : null
+      });
+    await Lot.create(
+      {
+        name: "Lot3",
+        description: "New Lot3",
+        quantity: 1,
+        locId: bay1.locationId,
+        price : "$50"
+      });
+    await Lot.create(
+      {
+        name: "Lot4",
+        description: "New Lot4",
+        quantity: 10,
+        locId: bay2.locationId,
+        price : "$10.99"
+      });
+    await Lot.create(
+      {
+        name: "Lot5",
+        description: "New Lot5",
+        quantity: 50,
+        locId: bay2.locationId,
+        price : null
+      });
+    await Lot.create(
+      {
+        name: "Lot6",
+        description: "New Lot6",
+        quantity: 300,
+        locId: bay2.locationId,
+        price : "$2"
+      });
+    await Lot.create(
+      {
+        name: "Lot7",
+        description: "New Lot7",
+        quantity: null,
+        locId: bay2.locationId,
+        price : "$30"
+      });
+    await Lot.create(
+      {
+        name: "Lot8",
+        description: "New Lot8",
+        quantity: null,
+        locId: studio.locationId,
+        price : null
+      });
+  } catch (error) {
+    
+  }
+};
+
+async function addProductions(){
+  try {
+    await Production.create({
+      title:"Carmen",
+      dateStart: new Date("May, 2020"),
+      dateEnd: new Date("July, 2020"),
+      active:true,
+      notes:"a test production"
+    });
+    await Production.create({
+      title:"La traviata",
+      dateStart: new Date("September, 2006"),
+      dateEnd: new Date("November, 2006"),
+      active:false,
+      notes:"a second test production"
+    });
+    await Production.create({
+      title:"The magic flute",
+      dateStart: new Date("January, 2001"),
+      dateEnd: new Date("December, 2003"),
+      active:true,
+      notes:"a third test production"
+    });
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+async function addProps(){
+  try {
+    const [lot1,lot2,lot3,lot4,lot5,lot6,lot7,lot8] = await Lot.findAll()
+    const [prod1,prod2,prod3] = await Production.findAll()
+
+    await Prop.create({
+      prodId:prod1.id,
+      lotId:lot1.id,
+      quantity:2,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod1.id,
+      lotId:lot5.id,
+      quantity:20,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod1.id,
+      lotId:lot3.id,
+      quantity:2,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod2.id,
+      lotId:lot8.id,
+      quantity:null,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod2.id,
+      lotId:lot6.id,
+      quantity:100,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod2.id,
+      lotId:lot1.id,
+      quantity:2,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod3.id,
+      lotId:lot3.id,
+      quantity:2,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod3.id,
+      lotId:lot7.id,
+      quantity:300,
+      notes:"prop notes"
+    })
+    await Prop.create({
+      prodId:prod3.id,
+      lotId:lot5.id,
+      quantity:10,
+      notes:"prop notes"
+    })
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+async function addTags(){
+  try {
+    const [lot1,lot2,lot3,lot4,lot5,lot6,lot7,lot8] = await Lot.findAll()
+
+    await Tag.create({title:"Set Dressing"})
+    await Tag.create({title:"Hand Prop"})
+    await Tag.create({title:"Florals"})
+    await Tag.create({title:"Blue"})
+    await Tag.create({title:"Red"})
+    await Tag.create({title:"Furniture"})
+    
+  } catch (error) {
+    
+  }
+}
+
+async function addUsers(){
+  try {
+    await User.register({
+      username: 'u1',
+      firstName: 'U1F',
+      lastName: 'U1L',
+      email: 'user1@user.com',
+      password: 'password1',
+      isAdmin: false
+    })
+    await User.register({
+      username: 'u2',
+      firstName: 'U2F',
+      lastName: 'U2L',
+      email: 'user2@user.com',
+      password: 'password2',
+      isAdmin: false
+    })
+    await User.register({
+      username: "u3",
+      firstName: "U3F",
+      lastName: "U3L",
+      email: "user3@user.com",
+      password: "password3",
+      isAdmin: false,
+    });
+    await User.register({
+      username: "a1",
+      firstName: "A1F",
+      lastName: "A1L",
+      email: "admin@user.com",
+      password: "adminpassword1",
+      isAdmin: true
+    });
+  } catch (error) {
+    console.error(error)
+  }
 };
 
 const u1Token = createToken({ username: 'u1', isAdmin: false })
