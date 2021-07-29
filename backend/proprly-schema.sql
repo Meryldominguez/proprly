1f78dc990d24c2bae22fdad45371b1ecc35a3dff
Meryl <meryldominguez@gmail.com>
2021-07-29 16:05:55 -0600

    sql tag_something function

--- backend/proprly-schema.sql
+++ backend/proprly-schema.sql
@@ -60,28 +60,43 @@ CREATE TABLE lot_tag (
   PRIMARY KEY (lot_id, tag_id)
 );
 
-CREATE OR REPLACE FUNCTION tag_something(lot_id INTEGER ,tag_title VARCHAR)
-RETURNS RECORD AS
+CREATE OR REPLACE FUNCTION tag_something(lot_id INTEGER,tag_title VARCHAR)
+RETURNS RECORD
+AS
 $$
 DECLARE
    new_tag_id INT;
    res RECORD;
+
   BEGIN
     IF NOT EXISTS (SELECT * FROM tag WHERE title=tag_title) 
     THEN
         INSERT INTO tag(title)
-        VALUES('test')
+        VALUES(tag_title)
         RETURNING id INTO new_tag_id;
 
         INSERT INTO lot_tag(lot_id,tag_id)
         VALUES(lot_id,new_tag_id)
-        RETURNING * INTO res;
+        RETURNING lot_tag.lot_id,lot_tag.tag_id INTO res;
+
+        SELECT l.id,l.name, t.id, t.title INTO res
+        FROM lot_tag AS x 
+        JOIN lot AS l ON l.id=x.lot_id
+        JOIN tag AS t ON t.id=x.tag_id
+        WHERE l.id=res.lot_id AND t.id=res.tag_id;
     ELSE
-        SELECT id INTO new_tag_id FROM tag WHERE title='test';
+        SELECT id INTO new_tag_id FROM tag WHERE title=tag_title;
 
         INSERT INTO lot_tag(lot_id,tag_id)
         VALUES(lot_id, new_tag_id)
-        RETURNING * INTO res;
+        RETURNING lot_tag.lot_id, lot_tag.tag_id INTO res;
+
+        SELECT l.id,l.name, t.id, t.title INTO res
+        FROM lot_tag AS x 
+        JOIN lot AS l ON l.id=x.lot_id
+        JOIN tag AS t ON t.id=x.tag_id
+        WHERE l.id=res.lot_id AND t.id=res.tag_id;
+
     END IF;
 RETURN res;
 END;
