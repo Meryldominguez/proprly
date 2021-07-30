f8de9340390167061a10c6685e8f28b77e8883cf
Meryl <meryldominguez@gmail.com>
2021-07-29 21:53:36 -0600

    /tags routes

--- backend/routes/tags.js
+++ backend/routes/tags.js
@@ -8,6 +8,7 @@ const { ensureAdmin, ensureLoggedIn } = require("../middleware/auth");
 const Tag = require("../models/tag");
 
 const tagSchema = require("../schemas/tagNewAndUpdate.json");
+const tagSearchSchema = require("../schemas/tagSearch.json");
 
 const router = new express.Router();
 
@@ -34,6 +35,68 @@ router.post("/",ensureLoggedIn, async function (req, res, next) {
     const tag = await Tag.create(req.body);
 
     return res.status(201).json({ tag });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST / { tag } =>  { tag }
+ *
+ * tag should be {  }
+ *
+ * Returns { id,  }
+ *
+ * Authorization required: logged in
+ */
+router.post("/lots/:lotId/",ensureLoggedIn, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, tagSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+    const tag = await Tag.tag(Number(req.params.lotId),req.body);
+
+    return res.status(201).json({ tag });
+  } catch (err) {
+    return next(err);
+  }
+});
+/** GET/[id]  =>  {  }
+ *
+ * Authorization: logged in
+ * 
+ */
+
+ router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const tag = await Tag.get(Number(req.params.id));
+    return res.json({ tag });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET/  =>  {  }
+ *
+ * Authorization: logged in
+ * 
+ */
+
+router.get("/",ensureLoggedIn, async function (req, res, next) {
+  try {
+    if (Object.keys(req.query).length>0){
+        const validator = jsonschema.validate(req.query, tagSearchSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+      const tags = await Tag.getAll(req.query.search);
+      return res.json({ tags });
+    }
+    const tags = await Tag.getAll();
+    return res.json({ tags });
+
   } catch (err) {
     return next(err);
   }
@@ -75,6 +138,20 @@ router.delete("/:id", ensureAdmin, async function (req, res, next) {
   try {
     const {id} = await Tag.remove(Number(req.params.id));
     return res.json({ deleted:id });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[id]  =>  { deleted: id }
+ *
+ * Authorization: admin
+ */
+
+router.delete("/lots/:tagId/:lotId", ensureAdmin, async function (req, res, next) {
+  try {
+    const lotTag = await Tag.removeTag(Number(req.params.lotId),Number(req.params.tagId));
+    return res.json({ deleted:lotTag });
   } catch (err) {
     return next(err);
   }
--- backend/routes/tags.test.js
+++ backend/routes/tags.test.js
@@ -26,164 +26,277 @@ afterAll(commonAfterAll)
 
 describe('POST /tags', function () {
   test('works for admin', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'The magic flute'`)
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot6'`)
-
     const resp = await request(app)
       .post('/tags')
       .send({
-          lotId: lot.id, 
-          prodId: prod.id,
-          quantity: 100,
-          notes:"a tag"
+          title: "Created"
       })
       .set('authorization', `Bearer ${adminToken}`)
     expect(resp.statusCode).toEqual(201)
     expect(resp.body).toEqual({
       tag: {
-        lotId: lot.id, 
-        prodId: prod.id,
-        quantity: 100,
-        notes:"a tag"
+        id: expect.any(Number),
+        title: "Created"
       }
     })
   })
-  test('works for user', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'The magic flute'`)
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot4'`)
 
+  test('works for user', async function () {
     const resp = await request(app)
       .post('/tags')
       .send({
-          lotId: lot.id, 
-          prodId: prod.id,
-          quantity: 100,
-          notes:"a tag"
+          title:"a tag"
       })
       .set('authorization', `Bearer ${u1Token}`)
     expect(resp.statusCode).toEqual(201)
     expect(resp.body).toEqual({
       tag: {
-        lotId: lot.id, 
-          prodId: prod.id,
-          quantity: 100,
-          notes:"a tag"
+        id: expect.any(Number),
+        title:"a tag"
       }
     })
   })
 
   test('unauth for anon', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'La traviata'`)
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot4'`)
-
     const resp = await request(app)
       .post('/tags')
       .send({
-        lotId: lot.id, 
-        prodId: prod.id,
-        quantity: 100,
-        notes:"a tag"
+        title:"a tag"
     })
     expect(resp.statusCode).toEqual(401)
   })
 
   test('bad request if missing data', async function () {
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot4'`)
     const resp = await request(app)
       .post('/tags')
       .send({
-          lotId: lot.id,
-          quantity: 100,
           notes:"a tag"
       })
       .set('authorization', `Bearer ${u1Token}`)
     expect(resp.statusCode).toEqual(400)
   })
 })
+/** ************************************ POST /tags/lots/:lotId */
 
-/** ************************************ PATCH /tags/:prodId/:lotId */
-
-describe('PATCH /tags/:id', () => {
-  test('works for admins', async function () {
-
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'La traviata'`)
+describe('POST /tags/lots/:lotId', function () {
+  test('works for admin, new tag', async function () {
     const { rows: [lot] } = await db.query(
       `SELECT * FROM lot
-        WHERE name = 'Lot8'`)
+        WHERE name = 'Lot6'`)
 
     const resp = await request(app)
-      .patch(`/tags/${prod.id}/${lot.id}`)
+      .post(`/tags/lots/${lot.id}`)
       .send({
-        notes:"New Notes"
+          title: "Created"
       })
       .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.statusCode).toEqual(201)
     expect(resp.body).toEqual({
       tag: {
-        prodId:prod.id,
-        lotId:lot.id,
-        quantity:null,
-        notes:"New Notes"
+        tagId: expect.any(Number),
+        tagTitle: "Created",
+        lotId: lot.id,
+        lotName: lot.name
+      }
+    })
+  })
+
+  test('works for admin, existing tag', async function () {
+    const { rows: [lot] } = await db.query(
+      `SELECT * FROM lot
+        WHERE name = 'Lot5'`)
+    const {body:{tag}} = await request(app)
+      .post('/tags')
+      .send({
+          title: "Created"
+      })
+      .set('authorization', `Bearer ${adminToken}`)
+
+    const resp = await request(app)
+      .post(`/tags/lots/${lot.id}`)
+      .send({
+          title: "Created"
+      })
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.statusCode).toEqual(201)
+    expect(resp.body).toEqual({
+      tag: {
+        tagId: tag.id,
+        tagTitle: "Created",
+        lotId: lot.id,
+        lotName: lot.name
       }
     })
   })
 
   test('works for user', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'Carmen'`)
     const { rows: [lot] } = await db.query(
       `SELECT * FROM lot
-        WHERE name = 'Lot5'`)
+        WHERE name = 'Lot4'`)
 
     const resp = await request(app)
-      .patch(`/tags/${prod.id}/${lot.id}`)
+      .post(`/tags/lots/${lot.id}`)
       .send({
-        quantity: 25
+          title:"Test"
       })
       .set('authorization', `Bearer ${u1Token}`)
+    expect(resp.statusCode).toEqual(201)
     expect(resp.body).toEqual({
       tag: {
-        prodId:prod.id,
-        lotId:lot.id,
-        quantity:25,
-        notes:"tag notes"
+        tagId: expect.any(Number),
+        tagTitle: "Test",
+        lotId: lot.id,
+        lotName: lot.name
       }
     })
   })
 
   test('unauth for anon', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'Carmen'`)
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot5'`)
-
     const resp = await request(app)
-      .patch(`/tags/${prod.id}/${lot.id}`)
+      .post('/tags/lots/1')
       .send({
-        notes: 'New'
+        title:"a tag"
+    })
+    expect(resp.statusCode).toEqual(401)
+  })
+
+  test('bad request if missing data', async function () {
+    const resp = await request(app)
+      .post('/tags/lots/1')
+      .send({
+      })
+      .set('authorization', `Bearer ${u1Token}`)
+    expect(resp.statusCode).toEqual(400)
+  })
+})
+
+/** ************************************ GET /tags/ */
+
+describe('GET /tags', ()=>{
+  test('Works for admins', async function(){
+    const resp = await request(app)
+      .get(`/tags`)
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.statusCode).toBe(200)
+    expect(resp.body.tags.length).toBe(6)
+  })
+  test('Works for users', async function(){
+    const resp = await request(app)
+      .get(`/tags`)
+      .set('authorization', `Bearer ${u1Token}`)
+    expect(resp.statusCode).toBe(200)
+    expect(resp.body.tags.length).toBe(6)
+  })
+  test('Works for admins with search', async function(){
+    const resp = await request(app)
+      .get(`/tags?search=set`)
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.statusCode).toBe(200)
+    expect(resp.body.tags.length).toBe(2)
+  })
+  test('Works for users with search', async function(){
+    const resp = await request(app)
+      .get(`/tags?search=set`)
+      .set('authorization', `Bearer ${u1Token}`)
+    expect(resp.statusCode).toBe(200)
+    expect(resp.body.tags.length).toBe(2)
+  })
+  test('unauth for anon', async function(){
+    const resp = await request(app)
+      .get(`/tags`)
+    expect(resp.statusCode).toEqual(401)
+  })
+})
+/** ************************************ GET /tags/:id */
+
+describe('GET /tags', ()=>{
+  test('Works for admins', async function(){
+    const { rows: [tag] } = await db.query(
+      `SELECT * FROM tag
+        WHERE title = 'Florals'`)
+    const resp = await request(app)
+      .get(`/tags/${tag.id}`)
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.statusCode).toBe(200)
+    expect(resp.body.tag).toEqual({
+      id: expect.any(Number),
+      title: "Florals",
+      lots: expect.any(Array)
+    })
+  });
+  test('Works for users', async function(){
+    const { rows: [tag] } = await db.query(
+      `SELECT * FROM tag
+        WHERE title = 'Florals'`)
+    const resp = await request(app)
+      .get(`/tags/${tag.id}`)
+      .set('authorization', `Bearer ${u1Token}`)
+    expect(resp.statusCode).toBe(200)
+    expect(resp.body.tag).toEqual({
+        id: expect.any(Number),
+        title: "Florals",
+        lots: expect.any(Array)
+      })
+    });
+  test('unauth for anon', async function(){
+    const resp = await request(app)
+      .get(`/tags/1`)
+    expect(resp.statusCode).toEqual(401)
+  });
+});
+
+/** ************************************ PATCH /tags/:id */
+
+describe('PATCH /tags/:id', () => {
+  test('works for admins', async function () {
+    const { rows: [tag] } = await db.query(
+      `SELECT * FROM tag
+        WHERE title = 'Hand Prop'`)
+    const resp = await request(app)
+      .patch(`/tags/${tag.id}`)
+      .send({
+        title:"New title"
+      })
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.body).toEqual({
+      tag: {
+        id: tag.id,
+        title:"New title"
+      }
+    })
+  })
+
+  test('works for user', async function () {
+    const { rows: [tag] } = await db.query(
+      `SELECT * FROM tag
+        WHERE title = 'Florals'`)
+    const resp = await request(app)
+      .patch(`/tags/${tag.id}`)
+      .send({
+        title:"New title"
+      })
+      .set('authorization', `Bearer ${u1Token}`)
+    expect(resp.body).toEqual({
+      tag: {
+        id: tag.id,
+        title:"New title"
+      }
+    })
+  })
+
+  test('unauth for anon', async function () {
+    const { rows: [tag] } = await db.query(
+      `SELECT * FROM tag
+        WHERE title = 'Blue'`)
+    const resp = await request(app)
+      .patch(`/tags/${tag.id}`)
+      .send({
+        title: 'New'
       })
     expect(resp.statusCode).toEqual(401)
   })
 
-  test('not found if no such production', async function () {
+  test('not found if no such tag', async function () {
     const resp = await request(app)
       .patch('/tags/1000000')
       .send({
@@ -202,50 +315,82 @@ describe('PATCH /tags/:id', () => {
   })
 
   test('bad request if invalid data', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'Carmen'`)
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot5'`)
+    const { rows: [tag] } = await db.query(
+      `SELECT * FROM tag
+        WHERE title = 'Blue'`)
     const resp = await request(app)
-      .patch(`/tags/${prod.id}/${lot.id}`)
+      .patch(`/tags/${tag.id}`)
       .send({
-        title: 42
+        qty: 42
       })
       .set('authorization', `Bearer ${adminToken}`)
     expect(resp.statusCode).toEqual(400)
   })
 })
 
-/** ************************************ DELETE /tags/:prodId/:lotId */
+/** ************************************ DELETE /tags/:id */
 
-describe('DELETE /tags/:prodId/:lotId', function () {
+describe('DELETE /tags/:id', function () {
+  test('works for admin', async function () {
+    const { rows: [tag] } = await db.query(`SELECT * FROM tag`)
+    const resp = await request(app)
+      .delete(`/tags/${tag.id}`)
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.body).toEqual({
+      deleted:tag.id
+    })
+    expect(resp.statusCode).toEqual(200)
+  });
+  
+  test('unauth for users', async function () {
+    const { rows: [tag] } = await db.query(`SELECT * FROM tag`)
+      const resp = await request(app)
+      .delete(`/tags/${tag.id}`)
+      .set('authorization', `Bearer ${u2Token}`)
+    expect(resp.statusCode).toEqual(401)
+  });
+
+  test('unauth for anon', async function () {
+    const { rows: [tag] } = await db.query(`SELECT * FROM tag`)
+      const resp = await request(app)
+      .delete(`/tags/${tag.id}`)
+    expect(resp.statusCode).toEqual(401)
+  });
+});
+
+/** ************************************ DELETE /tags/lots/:tagId/:lotId */
+
+describe('DELETE /tags/lots/:tagId/:lotId', function () {
+  test('works for admin', async function () {
+    const { rows: [{tag_id:tagId, lot_id:lotId}] } = await db.query(
+      `SELECT * FROM lot_tag`)
+    const resp = await request(app)
+      .delete(`/tags/lots/${tagId}/${lotId}`)
+      .set('authorization', `Bearer ${adminToken}`)
+    expect(resp.body).toEqual({
+      deleted:{
+        lotId,
+        tagId
+      }
+    })
+    expect(resp.statusCode).toEqual(200)
+  })
 
   test('unauth for users', async function () {
-    const { rows: [prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'The magic flute'`)
-      const { rows: [lot] } = await db.query(
-        `SELECT * FROM lot
-          WHERE name = 'Lot7'`)    
-
-    const resp = await request(app)
-      .delete(`/tags/${prod.id}/${lot.id}`)
+    const { rows: [{tag_id:tagId, lot_id:lotId}] } = await db.query(
+      `SELECT * FROM lot_tag`) 
+      const resp = await request(app)
+      .delete(`/tags/lots/${tagId}/${lotId}`)
       .set('authorization', `Bearer ${u2Token}`)
-
     expect(resp.statusCode).toEqual(401)
   })
 
   test('unauth for anon', async function () {
-    const { rows:[prod] } = await db.query(
-      `SELECT * FROM production
-        WHERE title = 'La traviata'`)
-    const { rows: [lot] } = await db.query(
-      `SELECT * FROM lot
-        WHERE name = 'Lot6'`)  
-    const resp = await request(app)
-      .delete(`/tags/${prod.id}/${lot.id}`)
+    const { rows: [{tag_id:tagId, lot_id:lotId}] } = await db.query(
+      `SELECT * FROM lot_tag`) 
+      const resp = await request(app)
+      .delete(`/tags/lots/${tagId}/${lotId}`)
+      
     expect(resp.statusCode).toEqual(401)
   })
 })
