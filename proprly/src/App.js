1c50fd7f28557c8e6210ff014186fe2e220696de
Meryl <meryldominguez@gmail.com>
2021-08-04 09:22:50 -0400

    App.js hides work in prod

--- proprly/src/App.js
+++ proprly/src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
+  if (process.env.NODE_ENV !=='production') {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Working on something cool.
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  //placeholder until up and running
   return (
     <div className="App">
       <header className="App-header">
